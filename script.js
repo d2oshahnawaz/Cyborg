86e04140ce0afc608cc6a05d7ab5eb30922c866b
Mohd Shahnawaz <83068798+d2oshahnawaz@users.noreply.github.com>
2026-06-06 04:59:09 +0530

    Add button hover effects and console log

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Cyborg Landing Page Loaded");
+
+document.querySelectorAll("button").forEach(btn=>{
+btn.addEventListener("mouseover",()=>{
+btn.style.transform="scale(1.05)";
+});
+
+btn.addEventListener("mouseout",()=>{
+btn.style.transform="scale(1)";
+});
+});
